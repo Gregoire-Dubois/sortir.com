d7294ee533c3c88a3cafd554bc649375138d1f83
Gregoire-Dubois <65712692+Gregoire-Dubois@users.noreply.github.com>
2023-07-26 11:08:41 +0200

    mise en place du menu déroulant avec les campus dans register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use function Sodium\add;
 
 class RegistrationFormType extends AbstractType
 {
@@ -22,8 +23,7 @@ class RegistrationFormType extends AbstractType
             ->add('prenom')
             ->add('pseudo')
             ->add('telephone')
-
-
+            ->add('campus')
 
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
