dbed9e1e229fa82589308c6b3c24f83901f8edcc
Cédric Crépin <ccrepin@gmail.com>
2023-07-26 13:01:27 +0200

    Essai d'affichage d'une Ville dans le formulaire Sortie mais échec pour le moment

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -6,14 +6,22 @@ use App\Entity\Campus;
 use App\Entity\Lieu;
 use App\Entity\Sortie;
 use App\Entity\Ville;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SortieType extends AbstractType
 {
+    private EntityManagerInterface $entityManager;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -31,6 +39,7 @@ class SortieType extends AbstractType
                 'format' => 'dd/MM/yyyyy',
                 //erreur Cannot use the "format" option of "Symfony\Component\Form\Extension\Core\Type\DateTimeType" when the "html5" option is enabled.
                 'html5' => false,
+                'widget' => 'single_text',
                 //'data' => 'tomorrow',
                 'help' => 'La date de clôture ne peut être supérieure à la date de sortie'
             ])
@@ -49,18 +58,28 @@ class SortieType extends AbstractType
                 'class' => Campus::class,
                 'choice_label' => 'nom'
             ])
-            ->add('ville', EntityType::class, [
+            ->add('ville', ChoiceType::class, [
                 'label' => 'Ville :',
-                'class' => Ville::class,
-                'choice_label' => 'nom'
+                'choices' => $this->getVillesChoices(),
+                //'choice_label' => 'nom'
             ])
             ->add('lieu', EntityType::class, [
                 'label' => 'Lieu :',
                 'class' => Lieu::class,
                 'choice_label' => 'nom'
             ])
-
         ;
+    }
+
+    private function getVillesChoices(): array
+    {
+        $villes = $this->entityManager->getRepository('App\Entity\Ville')->findAll();
+        $choices = [];
+
+        foreach ($villes as $ville) {
+            $choices[$ville->getNom()] = $ville->getId();
+        }
+        return $choices;
     }
     public function configureOptions(OptionsResolver $resolver): void
     {
