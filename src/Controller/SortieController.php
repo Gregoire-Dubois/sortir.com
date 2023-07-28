bc408a52003684fb1f75d26598758639a41245a3
pripop <p.popieul@laposte.net>
2023-07-28 15:44:13 +0200

    Inscription à une sortie Désistement à une sortie - Modification message d'erreur

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -136,10 +136,12 @@ class SortieController extends AbstractController
             $entityManager->flush();
             //dump($sortie);
             $this->addFlash('success', 'Vous êtes bien inscrit/e pour la sortie '.$sortie->getNom().' !');
+        }else{
+            $this->addFlash('error', 'Vous ne pouvez pas vous inscrire pour la sortie '.$sortie->getNom().' !');
         }
 
 
-        $this->addFlash('error', 'Vous ne pouvez pas vous inscrire pour la sortie '.$sortie->getNom().' !');
+
         //On renvoit vers l'URL qui a émis la requête
         if($url){
             return $this->redirect($url);
