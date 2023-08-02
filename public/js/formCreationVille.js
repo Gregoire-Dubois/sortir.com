e7382bfc4bafb5d7335cd1c859ebda3f71c6fda6
Tjosse <tanguy.josse2023@campus-eni.fr>
2023-08-02 17:33:23 +0200

    Ajout du CSS sur les Modals "Ajouter Ville" et "Ajouter Lieu"

--- public/js/formCreationVille.js
+++ public/js/formCreationVille.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Affichage du formulaire dans la modale
     document.getElementById("add_city_button").addEventListener("click", function (e) {
         e.preventDefault();
-
         // Effectuez une requête AJAX pour récupérer le contenu du formulaire
         let xhr = new XMLHttpRequest();
 
@@ -12,13 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
         xhr.onload = function () {
             if (xhr.status === 200) {
-                // Ajoutez le contenu de la modale dans le DOM
-                //console.log(xhr.responseText)
-                //console.log(xhr.status)
                 document.getElementById("popup_add_city").innerHTML = xhr.responseText;
-                //console.log(document.getElementById("popup_add_place").innerHTML)
                 // Affichez la modale
                 document.getElementById("popup_add_city").style.display = "block";
+                document.getElementById("popup_overlay").style.display = "block";
 
             } else {
                 console.error("Erreur lors de la requête AJAX");
@@ -37,10 +33,9 @@ document.addEventListener("DOMContentLoaded", function () {
             xhr.open("POST", "/sorties/ville/creation");
             xhr.onload = function () {
                 if (xhr.status === 200) {
-                    //console.log("Données enregistrées avec succès !");
-
                     // Cacher la modale après l'enregistrement
                     document.getElementById("popup_add_city").style.display = "none";
+                    document.getElementById("popup_overlay").style.display = "none";
 
                     // Mise à jour du formulaire de sortie avec la nouvelle ville
                     // Timeout pour laisser le temps à la base de créer comme il faut. Peut-être pas nécessaire
@@ -83,6 +78,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (e.target.id === 'cancel_add_city_button') {
                 e.preventDefault();
                 document.getElementById("popup_add_city").style.display = "none";
+                document.getElementById("popup_overlay").style.display = "none";
 
                 // Réaffecter les données déjà saisies du formulaire de sortie
                 for (let pair of sortieFormData.entries()) {
